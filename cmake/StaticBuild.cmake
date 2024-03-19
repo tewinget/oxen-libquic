03d84b63a195806fc43905e5029579cbb89661ef
Morgan Pretty <morgan.t.pretty@gmail.com>
2024-03-19 17:18:42 +1100

    Manually linking all of the dependencies to get iOS build working

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -13,6 +13,13 @@ set(GNUTLS_SOURCE gnutls-${GNUTLS_VERSION}.tar.xz)
 set(GNUTLS_HASH SHA512=74eddba01ce4c2ffdca781c85db3bb52c85f1db3c09813ee2b8ceea0608f92ca3912fd9266f55deb36a8ba4d01802895ca5d5d219e7d9caec45e1a8534e45a84
     CACHE STRING "gnutls source hash")
 
+set(LIBICONV_VERSION 1.17 CACHE STRING "libiconv version")
+set(LIBICONV_MIRROR ${LOCAL_MIRROR} https://ftp.gnu.org/gnu/libiconv
+    CACHE STRING "libiconv mirror(s)")
+set(LIBICONV_SOURCE libiconv-${LIBICONV_VERSION}.tar.gz)
+set(LIBICONV_HASH SHA512=18a09de2d026da4f2d8b858517b0f26d853b21179cf4fa9a41070b2d140030ad9525637dc4f34fc7f27abca8acdc84c6751dfb1d426e78bf92af4040603ced86
+    CACHE STRING "libiconv source hash")
+
 set(LIBUNISTRING_VERSION 1.1 CACHE STRING "libunistring version")
 set(LIBUNISTRING_MIRROR ${LOCAL_MIRROR} https://ftp.gnu.org/gnu/libunistring
     CACHE STRING "libunistring mirror(s)")
@@ -273,21 +280,29 @@ build_external(libtasn1
         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}"
         "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc}
     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libtasn1.a ${DEPS_DESTDIR}/include/libtasn1.h)
-add_static_target(libtasn1 libtasn1_external libtasn1.a)
+add_static_target(libtasn1::libtasn1 libtasn1_external libtasn1.a)
+
+build_external(libiconv
+    CONFIGURE_COMMAND ./configure ${gmp_build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
+        "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}"
+        "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc}
+    BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libiconv.a ${DEPS_DESTDIR}/include/iconv.h)
+add_static_target(libiconv::libiconv libiconv_external libiconv.a)
 
 build_external(libunistring
     CONFIGURE_COMMAND ./configure ${gmp_build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}"
         "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc}
+    DEPENDS libiconv_external
     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libunistring.a ${DEPS_DESTDIR}/include/unistr.h)
-add_static_target(libunistring libunistring_external libunistring.a)
+add_static_target(libunistring::libunistring libunistring_external libunistring.a libiconv::libiconv)
 
 build_external(libidn2
     CONFIGURE_COMMAND ./configure ${gmp_build_host} --disable-shared --disable-doc --prefix=${DEPS_DESTDIR} --with-pic
         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cflags_arch}" ${cross_rc}
     DEPENDS libunistring_external
     BUILD_BYPRODUCTS ${DEPS_DESTDIR}/lib/libidn2.a ${DEPS_DESTDIR}/include/idn2.h)
-add_static_target(libidn2 libidn2_external libidn2.a libunistring)
+add_static_target(libidn2::libidn2 libidn2_external libidn2.a libunistring::libunistring)
 
 build_external(gmp
     CONFIGURE_COMMAND ./configure ${gmp_build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
@@ -295,7 +310,7 @@ build_external(gmp
         "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}" ${cross_rc} CC_FOR_BUILD=cc CPP_FOR_BUILD=cpp
     DEPENDS libidn2_external libtasn1_external
 )
-add_static_target(gmp gmp_external libgmp.a libidn2 libtasn1)
+add_static_target(gmp::gmp gmp_external libgmp.a libidn2::libidn2 libtasn1::libtasn1)
 
 build_external(nettle
     CONFIGURE_COMMAND ./configure ${gmp_build_host} --disable-shared --prefix=${DEPS_DESTDIR} --libdir=${DEPS_DESTDIR}/lib
@@ -311,13 +326,14 @@ build_external(nettle
     ${DEPS_DESTDIR}/lib/libhogweed.a
     ${DEPS_DESTDIR}/include/nettle/version.h
 )
-add_static_target(nettle nettle_external libnettle.a gmp)
-add_static_target(hogweed nettle_external libhogweed.a nettle)
+add_static_target(nettle::nettle nettle_external libnettle.a gmp::gmp)
+add_static_target(hogweed::hogweed nettle_external libhogweed.a nettle::nettle)
 
 build_external(gnutls
     CONFIGURE_COMMAND ./configure ${gmp_build_host} --disable-shared --prefix=${DEPS_DESTDIR} --with-pic
         --without-p11-kit --disable-libdane --disable-cxx --without-tpm --without-tpm2 --disable-doc
         --without-zlib --without-brotli --without-zstd --without-libintl-prefix --disable-tests
+        --disable-valgrind-tests --disable-full-test-suite
         "PKG_CONFIG_PATH=${DEPS_DESTDIR}/lib/pkgconfig" "PKG_CONFIG=pkg-config"
         "CPPFLAGS=-I${DEPS_DESTDIR}/include" "LDFLAGS=-L${DEPS_DESTDIR}/lib${apple_ldflags_arch}"
         "CC=${deps_cc}" "CXX=${deps_cxx}" "CFLAGS=${deps_CFLAGS}${apple_cflags_arch}" "CXXFLAGS=${deps_CXXFLAGS}${apple_cxxflags_arch}" ${cross_rc}
@@ -326,7 +342,7 @@ build_external(gnutls
     ${DEPS_DESTDIR}/lib/libgnutls.a
     ${DEPS_DESTDIR}/include/gnutls/gnutls.h
 )
-add_static_target(gnutls::gnutls gnutls_external libgnutls.a hogweed)
+add_static_target(gnutls::gnutls gnutls_external libgnutls.a hogweed::hogweed)
 set(GNUTLS_FOUND ON CACHE BOOL "")
 set(GNUTLS_INCLUDE_DIR ${DEPS_DESTDIR}/include CACHE PATH "")
 set(GNUTLS_LIBRARY ${DEPS_DESTDIR}/lib/libgnutls.a CACHE FILEPATH "")
