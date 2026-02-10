4f74fc1db31dd817de90c2aa528946a3b58487eb
Jason Rhinelander <jason@imaginary.ca>
2026-02-10 16:24:15 -0400

    circumvent compiler narrowing conversion warnings

--- src/internal.hpp
+++ src/internal.hpp
@@ -36,10 +36,10 @@ namespace oxen::quic
 
     inline timeval loop_time_to_timeval(std::chrono::microseconds t)
     {
-#ifdef _WIN32
-        using suseconds_t = long;
-#endif
-        return timeval{.tv_sec = static_cast<time_t>(t / 1s), .tv_usec = static_cast<suseconds_t>((t % 1s) / 1us)};
+        timeval tv;
+        tv.tv_sec = t / 1s;
+        tv.tv_usec = (t % 1s) / 1us;
+        return tv;
     }
 
 }  // namespace oxen::quic
